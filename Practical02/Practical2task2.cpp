f0cb6de8f70525d2f36621c0251c1a631dcfb378
Ashutosh Uniyal <83587446+Ashuuniyal12@users.noreply.github.com>
2021-09-23 10:47:41 +0530

    Add files via upload

---
+++ Practical02/Practical2task2.cpp
@@ -0,0 +1,11 @@
+#include<iostream.h>
+using namespace std;
+
+int main()
+{
+	cout << "Hello";
+}
+/*WE are not suppose to use the .h header file 
+  as the preprocessor will not be able to find
+  the respective file and show fatal error 
+*/
