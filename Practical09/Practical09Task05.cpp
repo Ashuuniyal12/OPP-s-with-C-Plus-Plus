05d87efd9748e8d72c4dba95b210939ce3a70807
Ashutosh Uniyal <83587446+Ashuuniyal12@users.noreply.github.com>
2021-11-01 09:48:38 +0530

    Update Practical09Task05.cpp

--- Practical09/Practical09Task05.cpp
+++ Practical09/Practical09Task05.cpp
@@ -1,17 +1,17 @@
-/*Class : Provide Inheritance.												   |	Structure: Do not Provide Inheritance.
-			  Provide Encapsulation.											  |					  Do not Provide Encapsulation.
-			  Members are initilize using constructor.				 |					 Members are automatically initized.
-			  Variable can be initilize during decleration.		    |					 Variable can not be initilize during decleration.
-			  Memory to Object is provided in heap                 |		             Memory to variable is provided in Stack
+/*Class : Provide Inheritance.							|	Structure: Do not Provide Inheritance.
+			  Provide Encapsulation.				|	Do not Provide Encapsulation.
+			  Members are initilize using constructor.		|	Members are automatically initized.
+			  Variable can be initilize during decleration.		|	Variable can not be initilize during decleration.
+			  Memory to Object is provided in heap                  |	Memory to variable is provided in Stack
 */
 #include<iostream>
 using namespace std; 
 
 class College{			
-	string name;	 									//private variable name
+	string name;	 					//private variable name
 public: 	
 	static int strength ; 													
-	char sec;											//Class provides encaptulation because of access modifiers.
+	char sec;						//Class provides encaptulation because of access modifiers.
 };
 
 int College ::strength = 300;					// variable can be initialized during the declaration 
@@ -20,11 +20,11 @@ class College2 : public College{				// class can also be inherited.
 public:
 	College2() {cout << "Constructor called"<< endl;
 	cout << "All properties are inherited because class support inheritance"<< endl;} 
-																//to initialize the member of a class, the constructors and destructors are used 
+								//to initialize the member of a class, the constructors and destructors are used 
 };
 
 struct student{
-	int strenght =  300; 							// variables in the structure can not be initialized during the declaration 
+	int strenght =  300; 					// variables in the structure can not be initialized during the declaration 
 	string name;
 	char  sec;
 };
@@ -36,8 +36,8 @@ int main()
 	College2 d; 
 	
 	s.name = "Alex"; 
-	c.name = "Manan"; 								//compiler error because name is private
-	
+	c.name = "Manan"; 					//compiler error because name is private
+	return 0;
 }	
 		
 		
